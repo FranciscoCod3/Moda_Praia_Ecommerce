b670f220b2ac267d99d354e9c9e57cf10d8e968f
Francisco <dev.fr4ncisco@gmail.com>
2025-05-30 22:37:29 -0300

    Ajustos_Produtos_Header_tabela

--- Moda_Praia/Models/Produto.cs
+++ Moda_Praia/Models/Produto.cs
@@ -1,15 +1,23 @@
-﻿namespace Moda_Praia.Models
+﻿using System.ComponentModel;
+
+namespace Moda_Praia.Models
 {
     public class Produto
     {
+        [DisplayName("Identificador")]
         public int Id { get; set; }
         public string Nome { get; set; }
+        [DisplayName("Preço")]
         public decimal PrecoVenda { get; set; }
+        [DisplayName("Preço de Custo")]
         public decimal PrecoCusto { get; set; }
+        [DisplayName("Descrição do Produto")]
         public string Descricao { get; set; }
         public string Categoria { get; set; }
         public string urlName { get; set; }
+        [DisplayName("Quantidade")]
         public int? QuantidadeEstoque { get; set; }
+        [DisplayName("Cor Principal")]
         public string CorBase { get; set; }
     }
 }
