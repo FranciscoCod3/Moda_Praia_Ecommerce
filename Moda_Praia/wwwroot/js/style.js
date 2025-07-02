b3e5bf39b14179454c29eb5f4a92d603981992bb
Francisco <dev.fr4ncisco@gmail.com>
2025-07-02 15:23:11 -0300

    Ajustando Tailwindcss na Home e IndexHome

--- Moda_Praia/wwwroot/js/style.js
+++ Moda_Praia/wwwroot/js/style.js
@@ -1,35 +1,17 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
-    const descricaoContainer = document.querySelector('.descricao-container');
-    const botaoVerMais = document.querySelector('#verMais');
+﻿const vermais = document.getElementById('botaoVermais');
+const painel = document.getElementById('painel-recolher');
 
-    if (descricaoContainer && botaoVerMais) {
-        const alturaOriginal = '120px'; // Guardamos a altura original
-        const overflowOriginal = 'hidden';
-        let estadoExpandido = false; // Variável para rastrear o estado
-
-        botaoVerMais.addEventListener('click', function () {
-            if (!estadoExpandido) {
-                // Expandir
-                descricaoContainer.style.height = 'auto';
-                descricaoContainer.style.overflowY = 'auto';
-                descricaoContainer.classList.remove('fade-out'); // Remove a classe de fade
-                estadoExpandido = true;
-                botaoVerMais.textContent = 'ver menos'; // Mudar o texto do botão
-            } else {
-                // Voltar ao original
-                descricaoContainer.style.height = alturaOriginal;
-                descricaoContainer.style.overflowY = overflowOriginal;
-                descricaoContainer.classList.add('fade-out'); // Adiciona a classe de fade
-                estadoExpandido = false;
-                botaoVerMais.textContent = 'ver mais'; // Mudar o texto do botão de volta
-            }
-        });
-
-        // Adiciona a classe 'fade-out' inicialmente
-        descricaoContainer.classList.add('fade-out');
+vermais.addEventListener('click', () => {
+    const estaContraido = painel.classList.toggle('h-32');
+    if (estaContraido) {
+        painel.classList.remove('h-auto');
+        vermais.textContent = 'Ver mais';
+        console.log("Painel contraído.");
     } else {
-        console.log("Elemento '.descricao-container' ou botão '#verMais' não encontrado.");
+
+        painel.classList.add('h-auto');
+        vermais.textContent = 'Ver menos';
+        console.log("Painel expandido.");
     }
+
 });
-
-
